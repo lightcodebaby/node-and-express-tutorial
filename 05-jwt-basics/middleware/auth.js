489b7dd3c0fc82305ef7379498d039cb7ca1524d
rvbenlg <rulogo94@icloud.com>
2023-04-28 17:16:02 +0200

    lot of stuff

---
+++ 05-jwt-basics/middleware/auth.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const { UnauthorizedError } = require('../errors');
+
+const authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new UnauthorizedError('No token provided');
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const { id, username } = decoded;
+        req.user = { id, username };
+        next();
+    } catch (error) {
+        throw new UnauthorizedError('Not authorized to access this route');
+    }
+};
+
+module.exports = authMiddleware;
---
+++ jobs-app/errors/index.js
@@ -0,0 +1,10 @@
+'use strict';
+const CustomAPIError = require('./custom-error');
+const BadRequestError = require('./bad-request');
+const UnauthorizedError = require('./unauthorized');
+
+module.exports = {
+    CustomAPIError,
+    BadRequestError,
+    UnauthorizedError,
+};
