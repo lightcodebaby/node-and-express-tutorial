554db3b76d747c931eae9fb49ca127663379ef7e
rvbenlg <rulogo94@icloud.com>
2023-05-04 15:54:23 +0200

    more changes

--- 06-jobs-app/app.js
+++ 06-jobs-app/app.js
@@ -9,12 +9,21 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 
-// routes
-
 app.get('/', (req, res) => {
     res.send('jobs api');
 });
 
+// connect DB
+const connectDB = require('./db/connect');
+
+// routes
+const authRouter = require('./routes/authRoutes');
+const jobsRouter = require('./routes/jobsRoutes');
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+
+// error handler
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
@@ -22,6 +31,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, console.log(`Server is listening on port ${port}`));
     } catch (error) {
         console.log(error);
--- 06-jobs-app/middleware/error-handler.js
+++ 06-jobs-app/middleware/error-handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { CustomAPIError } = require('../errors');
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
     if (err instanceof CustomAPIError) {
